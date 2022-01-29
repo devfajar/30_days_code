0d744895e9001073da75b491149920db4a42c67e
devfajar <fajartrengginas@gmail.com>
2022-01-29 15:17:09 +0800

    30 days code go to finishing

---
+++ exception.py
@@ -0,0 +1,18 @@
+# Day 16
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+S = input().strip()
+
+try:
+    S = int(S)
+    print(S)
+except:
+    print("Bad String")
